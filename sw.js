a68ff51f6995749796b00129077725f31d0db0da
srbirabarbearia <melojunior0@gmail.com>
2026-08-25 11:22:18 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 const CACHE_NAME = 'barbearia-v1';
 const assets = ['./', './manifest.json'];
 
-// Instalação do Service Worker e armazenamento em cache básico
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -10,7 +9,6 @@ self.addEventListener('install', e => {
   );
 });
 
-// Ativação e limpeza de caches antigos
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => {
@@ -25,7 +23,6 @@ self.addEventListener('activate', e => {
   );
 });
 
-// Estratégia de requisição: tenta a rede primeiro, se falhar busca no cache
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => {
